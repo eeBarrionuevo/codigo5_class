cf2dc83951a8a87ca909cd54336542b39aeb9162
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-08-05 12:25:20 -0500

    Mods

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:codigo5_class/person.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,10 +14,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   List<String> names = [
-    "Luis 987123222 luis@gmail.com",
-    "Eddy ",
+    "Eddy",
     "Carlos ",
     "Isabel ",
     "Jose",
@@ -24,13 +29,24 @@ class HomePage extends StatelessWidget {
     "Adrian",
   ];
 
-  List<Map<String, dynamic>> people = [
-    {"name": "Adrian", "address": "Av. Cayma 123", "phone": "987122333"},
-    {"name": "Eddy", "address": "Av. Lima 6565", "phone": "987122666"},
-    {"name": "Isabel", "address": "Av. Torres 333", "phone": "987122222"},
-    {"name": "Adrian", "address": "Av. Cayma 123", "phone": "987122333"},
-    {"name": "Eddy", "address": "Av. Lima 6565", "phone": "987122666"},
-    {"name": "Isabel", "address": "Av. Torres 333", "phone": "987122222"},
+  Person pepe = Person(name: "José", address: "Av. Lima", phone: "12323");
+  Person manolo = Person(name: "José", address: "Av. Lima", phone: "12323");
+  Person fiorella = Person(name: "José", address: "Av. Lima", phone: "12323");
+  Person luis = Person(name: "José", address: "Av. Lima", phone: "12323");
+  Person elvis = Person(name: "José", address: "Av. Lima", phone: "12323");
+
+  // List<Map<String, dynamic>> people = [
+  //   {"name": "Adrian", "address": "Av. Cayma 123", "phone": "987122333"},
+  //   {"name": "Eddy", "address": "Av. Lima 6565", "phone": "987122666"},
+  //   {"name": "Isabel", "address": "Av. Torres 333", "phone": "987122222"},
+  // ];
+
+  List<Person> people = [
+    Person(name: "Juan", address: "Av. Lima", phone: "12321323"),
+    Person(name: "Juan", address: "Av. Lima", phone: "12321323"),
+    Person(name: "Juan", address: "Av. Lima", phone: "12321323"),
+    Person(name: "Juan", address: "Av. Lima", phone: "12321323"),
+    Person(name: "Juan", address: "Av. Lima", phone: "12321323"),
   ];
 
   @override
@@ -43,18 +59,28 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              //Agregar una nueva persona
+              // people.add(
+              //   {
+              //     "name": "Pedro",
+              //     "address": "Cll Torres 123",
+              //     "phone": "987111222",
+              //   },
+              // );
+              setState(() {});
+            },
             child: Text("Agregar"),
           ),
           ...people
               .map(
                 (e) => ListTile(
                   leading: CircleAvatar(
-                    child: Text(e["name"][0]),
+                    child: Text(e.name[0]),
                   ),
-                  title: Text(e["name"]),
-                  subtitle: Text(e["address"]),
-                  trailing: Text(e["phone"]),
+                  title: Text(e.name),
+                  subtitle: Text(e.address),
+                  trailing: Text(e.phone),
                 ),
               )
               .toList(),
---
+++ lib/person.dart
@@ -0,0 +1,12 @@
+//Models
+class Person {
+  String name;
+  String address;
+  String phone;
+
+  Person({
+    required this.name,
+    required this.address,
+    required this.phone,
+  });
+}
